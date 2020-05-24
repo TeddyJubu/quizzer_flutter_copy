3e187d8bd09d1911d9ca1eba966a22792ef2ff37
TeddyJubu <syeedmdjobayer@gmail.com>
2020-05-24 16:53:35 +0600

    conditionals introduced

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,21 +31,34 @@ class Quizzer extends StatefulWidget {
 }
 
 class _QuizzerState extends State<Quizzer> {
+  String temp;
+  int count = 0;
+  List<String> statement = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answer = [false, true, true];
+  List<Icon> scoreKeeper = [];
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
           flex: 5,
-          child: Container(
-            child: Center(
-              child: Text(
-                'Question',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              child: Center(
+                child: Text(
+                  statement[count],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
@@ -55,9 +69,25 @@ class _QuizzerState extends State<Quizzer> {
             padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               color: Colors.green.shade500,
-              onPressed: () {},
+              onPressed: () {
+                bool correctAnswer = answer[count];
+                if (correctAnswer == true) {
+                  print("right answer");
+                } else {
+                  print('wrong answer');
+                }
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                  count++;
+                });
+              },
               child: Text(
-                'Yes',
+                'True',
                 style: TextStyle(
                   fontSize: 20,
                   color: Colors.white,
@@ -71,9 +101,25 @@ class _QuizzerState extends State<Quizzer> {
             padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               color: Colors.red.shade500,
-              onPressed: () {},
+              onPressed: () {
+                bool correctAnswer = answer[count];
+                if (correctAnswer == false) {
+                  print("right answer");
+                } else {
+                  print('wrong answer');
+                }
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                  count++;
+                });
+              },
               child: Text(
-                'No',
+                'False',
                 style: TextStyle(
                   fontSize: 20,
                   color: Colors.white,
@@ -82,6 +128,9 @@ class _QuizzerState extends State<Quizzer> {
             ),
           ),
         ),
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
